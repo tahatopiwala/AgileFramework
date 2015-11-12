c233ea85f2beb7856d3db67ea86da01a231143f4
Taha Topiwala <Taha@tahas-imac.local>
2015-11-12 17:29:39 -0500

    Main Files

---
+++ AgileFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgileFramework
+//
+//  Created by Taha Topiwala on 11/12/15.
+//  Copyright © 2015 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
